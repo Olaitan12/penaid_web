a32c2415d41de5b5b2c7a982b8e4d918cc95f492
olaitan12 <olaitanaderoju@gmail.com>
2020-12-17 14:53:33 +0100

    Build

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -38,23 +38,23 @@ export const configuration  = {
     testimonials: {
         tagline: 'Testimonials',
         title: 'What Client Says, ',
-        description: 'We are very fortunate to have formed excellent partnerships with many of our clients. And we’ve formed more than just working relationships with them; we have formed true friendships. Here’s what they’re saying about us.',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti similique voluptatum illo aut, delectus necessitatibus maiores! Corporis cumque nulla optio dolorum id accusamus amet! Quo adipisci aspernatur inventore maiores esse!',
         feedbacks: [
             { name: 'John Obi', 
               userimage: 'author-1.jpg', 
-              comments: '“Penaid has always helped pensioners in need and continue helping us. You will not regret using their services!”',
+              comments: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti similique voluptatum illo aut, delectus necessitatibus maiores! Corporis cumque nulla optio dolorum id accusamus amet! Quo adipisci aspernatur inventore maiores esse!',
               location: 'Imo State Pensioner'
             },
 
             { name: 'Mariam Agbabiaka', 
               userimage: 'author-2.jpg', 
-              comments: '“Penaid has helped me pay my kids school fees and be able to sort my house rent in due time. They are fantastic.”',
+              comments: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti similique voluptatum illo aut, delectus necessitatibus maiores! Corporis cumque nulla optio dolorum id accusamus amet! Quo adipisci aspernatur inventore maiores esse!',
               location: 'Ogun State Pensioner'
             },
 
             { name: 'Sulaimon Danjuma', 
               userimage: 'author-3.jpg', 
-              comments: "“I just got onboarded at Penaid, the registration process was not bad at all just get the necessary documents and you're good to go.”",
+              comments: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti similique voluptatum illo aut, delectus necessitatibus maiores! Corporis cumque nulla optio dolorum id accusamus amet! Quo adipisci aspernatur inventore maiores esse!",
               location: 'Ibadan Pensioner'
             },
         ],
